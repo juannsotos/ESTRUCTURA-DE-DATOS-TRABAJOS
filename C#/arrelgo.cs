1554e5948bd0b823528afc7f63a3b83add60299e
juannsotos <juannsotos@gmail.com>
2025-08-31 22:42:20 -0700

    agg c# y python

---
+++ C#/arrelgo.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        int[] enteros = {0,1,2,3};
+        int[] numeros = new int[3];
+
+
+        Console.WriteLine(enteros[1]); 
+        Console.WriteLine(enteros[2]);
+        Console.WriteLine(enteros[3]);
+        
+
+        Console.ReadKey();
+    }
+}
---
+++ C++/arreglos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int numeros[5];
+
+numeros[1] = 14;
+numeros[0] = 1;
+
+cout << numeros[0] << endl;
+
+int otros[3] = {123,54,34};
+cout << otros[2] << endl;
+
+return 0;
+}
