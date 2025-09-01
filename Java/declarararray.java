e34b522642512e505346fe119dda8d37b6b7073d
juannsotos <juannsotos@gmail.com>
2025-08-31 23:07:46 -0700

    Create declarararray.java

---
+++ Java/declarararray.java
@@ -0,0 +1,15 @@
+public class declarararray {
+    public static void main(String[] args) {
+        int[]  numeros = new int[5];
+
+        numeros[0] = 122;
+        numeros[1] = 19;
+
+        System.out.println(numeros[0]);
+
+        int[] otros = {0,1,2,3};
+        System.out.println(otros[3]);
+
+    }
+    
+}
