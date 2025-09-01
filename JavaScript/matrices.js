48e765da7a66fcfbf59ef44a2360430035a1a153
juannsotos <juannsotos@gmail.com>
2025-08-31 22:59:02 -0700

    Create matrices.js

---
+++ JavaScript/matrices.js
@@ -0,0 +1,13 @@
+//Declarar arreglo
+let numeros = []
+
+//Meter datos al arreglo
+numeros[0] = 13
+numeros[1] = 15
+let otros = [10,2,5]
+
+//Mostrar un valor
+console.log(otros[2])
+console.log("hola")
+
+console.log(numeros[0])
