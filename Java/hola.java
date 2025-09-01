9e407452e2cad77e60bd338c67e41f3998d1453b
juannsotos <juannsotos@gmail.com>
2025-08-31 21:56:48 -0700

    la cague

--- Java/hola.java
+++
@@ -1,8 +0,0 @@
-package Java;
-
-public class hola {
-    public static void main(String[] args) {
-        System.out.println("q hay chavalin");
-    }
-    
-}
--- Java/otronuevo.java
+++
@@ -1,9 +0,0 @@
-package Java;
-
-public class otronuevo {
-    public static void main(String[] args) {
-        System.out.println("vamos viendo este");
-        
-    }
-    
-}
