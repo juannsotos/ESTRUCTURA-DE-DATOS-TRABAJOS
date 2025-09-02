7d93a569ba12da0c251d2b47e1cfa62a17ae9aa3
juannsotos <juannsotos@gmail.com>
2025-09-01 21:32:10 -0700

    ahorita veo

---
+++ Java/recorrer.java
@@ -0,0 +1,12 @@
+public class recorrer {
+    public static void main(String[] args) {
+        int[] a = {10,20,30,400,5};
+
+    for (int i = 0; i < a.length; i++){
+        System.err.println("Posicion " + i + "Valor " + a[i]);
+    }
+    }
+    
+
+    
+}
---
+++ JavaScript/recorrerarray.js
@@ -0,0 +1,5 @@
+a = [2,45,4,6,3]
+
+for (let i =0; i < a.length; i++){
+    console.log("Posicion: ", i, ' valor: ', a[i])
+}
