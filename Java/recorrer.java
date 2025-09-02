93ea8974e583d9623020b0738692565ac22f129a
juannsotos <juannsotos@gmail.com>
2025-09-02 00:25:36 -0700

    va 1 faltan 4

--- Java/recorrer.java
+++ Java/recorrer.java
@@ -3,7 +3,7 @@ public class recorrer {
         int[] a = {10,20,30,400,5};
 
     for (int i = 0; i < a.length; i++){
-        System.err.println("Posicion " + i + "Valor " + a[i]);
+        System.err.println("Posicion " + i + " Valor " + a[i]);
     }
     }
     
--- Python/recorrerarray.py
+++ Python/recorrerarray.py
@@ -1,5 +1,12 @@
 a = [12,15,18,334,523,34]
+
 print("Recorrido Inverso del array: ", end="")
 print(" \nLos elementos del array son: ",end="")
-for i in range(len(a)-1,-1,-1): # el primero es pa quitarle 1 al range y el otro es pa saber donde se va detener el ciclo y el ultimo es el paso con el que lo va a hacer
+
+for i in range(len(a)-1,1,1): # el primero es pa quitarle 1 al range y el otro es pa saber donde se va detener el ciclo y el ultimo es el paso con el que lo va a hacer
     print (a[i], "", end="")
+
+
+
+
+
